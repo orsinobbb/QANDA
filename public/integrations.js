450166666384b5a2b36a353ce77253b0a434e4c3
orsino <orsino.tseng@fusheng.com.tw>
2026-08-13 14:58:25 +0800

    Configure Google Sheets result receiver

--- public/integrations.js
+++ public/integrations.js
@@ -1,3 +1,3 @@
 window.QANDA_CONFIG = Object.freeze({
-  googleAppsScriptUrl: ""
+  googleAppsScriptUrl: "https://script.google.com/macros/s/AKfycbwf0wgQEuQjGL_Xdyl8R56_ZthIYLGAiCJjgjsuhsdBQ752rD6QErav8o38EWh8KeCUPQ/exec"
 });
